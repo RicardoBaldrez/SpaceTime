aaa608aeb29442d25904141fde0006059d579806
Ricardo Baldrez <ricardo.baldrez@gmail.com>
2023-05-15 23:42:54 -0300

    refactor: 💡 TailWind example

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div>
-      Initial Page
+    <div className='h-screen bg-zinc-950 text-zinc-50'>
+      <h1 className='text-4xl font-bold'>Sua cápsula do tempo</h1>
     </div>
   )
 }

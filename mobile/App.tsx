5a6070e8fd996532b9c6c07de5a50d4e75644bf6
Ricardo Baldrez <ricardo.baldrez@gmail.com>
2023-05-17 17:58:52 -0300

    feat: 🎸 Home page finalized

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar'
-import { Text, View } from 'react-native'
+import { ImageBackground, View, Text, TouchableOpacity } from 'react-native'
 
 import {
   useFonts,
@@ -8,6 +8,14 @@ import {
 } from '@expo-google-fonts/roboto'
 
 import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+
+import blurBg from './src/assets/bg-blur.png'
+
+import Stripes from './src/assets/stripes.svg'
+import Logo from './src/assets/spacetime-logo.svg'
+import { styled } from 'nativewind'
+
+const StyledStripes = styled(Stripes)
 
 export default function App() {
   const [hasLoadedFonts] = useFonts({
@@ -19,11 +27,40 @@ export default function App() {
   if (!hasLoadedFonts) return null
 
   return (
-    <View className="flex-1 items-center justify-center bg-gray-900">
-      <Text className="font-title text-5xl font-bold text-gray-50">
-        Rocketseat
+    <ImageBackground
+      source={blurBg}
+      imageStyle={{ position: 'absolute', left: '-120%' }}
+      className="relative flex-1 items-center bg-gray-900 px-8 py-10"
+    >
+      <StyledStripes className="absolute left-2" />
+
+      <View className="flex-1 items-center justify-center gap-6">
+        <Logo />
+        <View className="space-y-2">
+          <Text className="text-center font-title text-2xl leading-tight text-gray-50">
+            Sua cápsula do tempo
+          </Text>
+          <Text className="text-center font-body text-base leading-relaxed text-gray-100">
+            Colecione momentos marcantes da sua jornada e compartilhe (se
+            quiser) com o mundo!
+          </Text>
+        </View>
+
+        <TouchableOpacity
+          activeOpacity={0.7}
+          className="rounded-full bg-green-500 px-5 py-2"
+        >
+          <Text className="font-alt text-sm uppercase text-black">
+            Cadastrar lembrança
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
+        Feito com 💜 no NLW da Rocketseat
       </Text>
+
       <StatusBar style="light" translucent />
-    </View>
+    </ImageBackground>
   )
 }

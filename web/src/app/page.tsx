e9e063008cfe2c17b8996746f62feb037dfa8ee5
Ricardo Baldrez <ricardo.baldrez@gmail.com>
2023-05-18 14:28:02 -0300

    feat: 🎸 OAuth Github configured and getting code github web

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
 
 
       {/** Sign in */}
-      <a href="" className='flex items-center gap-3 text-left hover:text-gray-50 transition-colors'>
+      <a href={`https://github.com/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID}`} className='flex items-center gap-3 text-left hover:text-gray-50 transition-colors'>
         <div className='flex h-10 w-10 items-center justify-center rounded-full bg-gray-400'>
           <User className='h-5 w-5 text-gray-500' />
         </div>

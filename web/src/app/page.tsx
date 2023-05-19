34a22032842fea246a2b629abcece8d41485de3a
Ricardo Baldrez <ricardo.baldrez@gmail.com>
2023-05-19 03:17:37 -0300

    refactor: 💡 Displaying the logged in user's profile
    Displaying the logged in user's profile, get user function through
    cookie, configured image domain

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,24 +1,31 @@
+import { cookies } from 'next/headers'
+
 import { Copyright } from '@/components/Copyright'
 import { EmptyMemories } from '@/components/EmptyMemories'
 import { Hero } from '@/components/Hero'
 import { SignIn } from '@/components/SignIn'
+import { Profile } from '@/components/Profile'
 
 export default function Home() {
-  return <main className='grid grid-cols-2 min-h-screen'>
-    <div className='relative flex flex-col items-start justify-between px-28 py-16 overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover'>
-      {/** Blur */}
-      <div className='absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 bg-purple-700 rounded-full opacity-50 blur-full' />
+  const isAuthenticated = cookies().has('token')
 
-      {/** Stripes */}
-      <div className='absolute right-2 top-0 bottom-0 w-2 pr-2 bg-stripes' />
+  return (
+    <main className="grid min-h-screen grid-cols-2">
+      <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
+        {/** Blur */}
+        <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
 
-      <SignIn />
-      <Hero />
-      <Copyright />
-    </div>
+        {/** Stripes */}
+        <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes pr-2" />
 
-    <div className="flex flex-col p-16 bg-[url(../assets/bg-stars.svg)] bg-cover">
-      <EmptyMemories />
-    </div>
-  </main>
+        {isAuthenticated ? <Profile /> : <SignIn />}
+        <Hero />
+        <Copyright />
+      </div>
+
+      <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
+        <EmptyMemories />
+      </div>
+    </main>
+  )
 }

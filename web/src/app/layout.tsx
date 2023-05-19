6a2cb080628ce80cd7c5cd105898b277c3fdb6b4
Ricardo Baldrez <ricardo.baldrez@gmail.com>
2023-05-19 16:44:15 -0300

    refactor: 💡 Route layouts defined

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,8 +1,18 @@
+import { Hero } from '@/components/Hero'
 import './globals.css'
 import { Roboto_Flex, Bai_Jamjuree } from 'next/font/google'
+import { Profile } from '@/components/Profile'
+import { EmptyMemories } from '@/components/EmptyMemories'
+import { SignIn } from '@/components/SignIn'
+import { Copyright } from '@/components/Copyright'
+import { cookies } from 'next/headers'
 
 const roboto = Roboto_Flex({ subsets: ['latin'], variable: '--font-roboto' })
-const baiJam = Bai_Jamjuree({ subsets: ['latin'], weight: '700', variable: '--font-bai-jamjuree' })
+const baiJam = Bai_Jamjuree({
+  subsets: ['latin'],
+  weight: '700',
+  variable: '--font-bai-jamjuree',
+})
 
 export const metadata = {
   title: 'NLW SpaceTime',
@@ -12,11 +22,34 @@ export const metadata = {
 export default function RootLayout({
   children,
 }: {
+  // eslint-disable-next-line no-undef
   children: React.ReactNode
 }) {
+  const isAuthenticated = cookies().has('token')
+
   return (
     <html lang="en">
-      <body className={`${roboto.variable} ${baiJam.variable} font-sans bg-gray-900 text-gray-100`}>{children}</body>
+      <body
+        className={`${roboto.variable} ${baiJam.variable} bg-gray-900 font-sans text-gray-100`}
+      >
+        <main className="grid min-h-screen grid-cols-2">
+          <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
+            {/** Blur */}
+            <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
+
+            {/** Stripes */}
+            <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes pr-2" />
+
+            {isAuthenticated ? <Profile /> : <SignIn />}
+            <Hero />
+            <Copyright />
+          </div>
+
+          <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
+            {children}
+          </div>
+        </main>
+      </body>
     </html>
   )
 }

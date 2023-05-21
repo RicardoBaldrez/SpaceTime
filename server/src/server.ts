7fb84b71393cbfb92a1160e8874ae040deb09a6b
Ricardo Baldrez <ricardo.baldrez@gmail.com>
2023-05-20 21:33:02 -0300

    refactor: 💡 File uploads route created and serving static files

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,20 +1,35 @@
 import 'dotenv/config'
 
+import { resolve } from 'node:path'
+
 import fastify from 'fastify'
 import jwt from '@fastify/jwt'
 import cors from '@fastify/cors'
-import { memoriesRoutes } from './routes/memories'
+import multipart from '@fastify/multipart'
+
 import { authRoutes } from './routes/auth'
+import { uploadRoutes } from './routes/uploads'
+import { memoriesRoutes } from './routes/memories'
 
 const app = fastify()
+
+app.register(multipart)
+
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
 
 app.register(cors, {
   origin: true,
 })
+
 app.register(jwt, {
   secret: 'spacetime',
 })
+
 app.register(authRoutes)
+app.register(uploadRoutes)
 app.register(memoriesRoutes)
 
 app

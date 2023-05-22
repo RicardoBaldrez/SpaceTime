5f46d5406323cc574d7ace3fe9d6bd32f95713d5
Ricardo Baldrez <ricardo.baldrez@gmail.com>
2023-05-21 22:04:06 -0300

    refactor: 💡 Selecting image from gallery and Image preview

--- mobile/app/new.tsx
+++ mobile/app/new.tsx
@@ -7,18 +7,45 @@ import {
   View,
   TextInput,
   ScrollView,
+  Image,
 } from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
 import { Link } from 'expo-router'
 import Icon from '@expo/vector-icons/Feather'
+import * as ImagePicker from 'expo-image-picker'
 
 import NLWLogo from '../src/assets/spacetime-logo.svg'
 
 export default function NewMemorie() {
   const { top, bottom } = useSafeAreaInsets()
 
+  const [content, setContent] = useState('')
   const [isPublic, setIsPublic] = useState(false)
+  const [preview, setPreview] = useState<string | null>(null)
+
+  async function openImagePicker() {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+      })
+
+      if (result.assets[0]) {
+        setPreview(result.assets[0].uri)
+      }
+    } catch (error) {
+      console.log(error)
+    }
+
+    // if (!result.canceled) {
+    //   setImage(result.assets[0].uri)
+    // }
+  }
+
+  function handleCreateMemory() {
+    console.log(content, isPublic)
+  }
 
   return (
     <ScrollView
@@ -27,6 +54,7 @@ export default function NewMemorie() {
     >
       <View className="mt-4 flex-row items-center justify-between">
         <NLWLogo />
+
         <Link href="/memories" asChild>
           <TouchableOpacity className="h-10 w-10 items-center justify-center rounded-full bg-purple-500">
             <Icon name="arrow-left" size={16} color="#fff" />
@@ -49,24 +77,36 @@ export default function NewMemorie() {
 
         <TouchableOpacity
           activeOpacity={0.7}
+          onPress={openImagePicker}
           className="h-32 items-center justify-center rounded-lg border border-dashed border-gray-500 bg-black/20"
         >
-          <View className="flex-row items-center gap-2">
-            <Icon name="image" color="#fff" />
-            <Text className="font-body text-sm text-gray-200">
-              Adicionar foto ou vídeo de capa
-            </Text>
-          </View>
+          {preview ? (
+            <Image
+              alt=""
+              source={{ uri: preview }}
+              className="h-full w-full rounded-lg object-cover"
+            />
+          ) : (
+            <View className="flex-row items-center gap-2">
+              <Icon name="image" color="#fff" />
+              <Text className="font-body text-sm text-gray-200">
+                Adicionar foto ou vídeo de capa
+              </Text>
+            </View>
+          )}
         </TouchableOpacity>
 
         <TextInput
           multiline
-          className="p-0 font-body text-lg text-gray-50"
+          value={content}
+          onChangeText={setContent}
           placeholderTextColor="#56565a"
+          className="p-0 font-body text-lg text-gray-50"
           placeholder="Fique livre para adicionar fotos, vídeos e relatos sobre essa experiência que você quer lembrar para sempre."
         />
 
         <TouchableOpacity
+          onPress={handleCreateMemory}
           activeOpacity={0.7}
           className="mb-3 items-center self-end rounded-full bg-green-500 px-5 py-2"
         >
